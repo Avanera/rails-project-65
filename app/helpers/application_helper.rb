5c9c7f4e00f4ca07f3d50a82ec7d87b79c2424b1
Arina Zaitseva <avanero@bk.ru>
2024-09-15 10:44:59 -0400

    add render flash

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,13 @@
 module ApplicationHelper
+  def get_flash_class_for(type)
+    {
+      success: 'alert alert-success',
+      error: 'alert alert-danger',
+      alert: 'alert alert-warning',
+      notice: 'alert alert-info'
+    }[type.to_sym]
+  end
+
   def assign_provider
     if Rails.env == 'development'
       provider = 'developer'
