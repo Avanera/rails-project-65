1c44ba5af6847f25772fa55e149dc68fd8e7f0d7
Arina Zaitseva <avanero@bk.ru>
2024-09-18 22:02:57 -0400

    add limited seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+MIN_BULLETINS_COUNT = 150
+
 [
   'Недвижимость',
   'Транспорт',
@@ -10,4 +12,24 @@
   'Другое'
 ].each do |name|
   Category.find_or_create_by!(name:)
+end
+
+if Bulletin.count < MIN_BULLETINS_COUNT
+  files = Dir[Rails.root.join('test/fixtures/files/*')]
+
+  (MIN_BULLETINS_COUNT - Bulletin.count).times do |i|
+    bulletin = Bulletin.new(
+      title: Faker::Food.dish,
+      description: Faker::Food.description,
+      user_id: User.all.sample.id,
+      category_id: Category.all.sample.id
+    )
+
+    file = File.open(files.sample)
+    bulletin.image.attach(io: file, filename: File.basename(file), content_type: 'image/jpg')
+    bulletin.save!
+    file.close
+    bulletin.to_moderate!
+    bulletin.publish!
+  end
 end
