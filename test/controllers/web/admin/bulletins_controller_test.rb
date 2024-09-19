69aa85184490e6e48607ab39ec5b8d8f74144e31
Arina Zaitseva <avanero@bk.ru>
2024-09-19 17:27:50 -0400

    add tests to Web::Admin::BulletinsController

---
+++ test/controllers/web/admin/bulletins_controller_test.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class Web::Admin::BulletinsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    sign_in users(:admin)
+    @bulletin = bulletins(:under_moderation)
+  end
+
+  test 'should get index' do
+    get admin_bulletins_url
+
+    assert_response :success
+  end
+
+  test 'should archive' do
+    patch archive_admin_bulletin_path(@bulletin)
+
+    @bulletin.reload
+    assert_equal('archived', @bulletin.state)
+  end
+
+  test 'should publish' do
+    patch publish_admin_bulletin_path(@bulletin)
+
+    @bulletin.reload
+    assert_equal('published', @bulletin.state)
+  end
+
+  test 'should reject' do
+    patch reject_admin_bulletin_path(@bulletin)
+
+    @bulletin.reload
+    assert_equal('rejected', @bulletin.state)
+  end
+end
