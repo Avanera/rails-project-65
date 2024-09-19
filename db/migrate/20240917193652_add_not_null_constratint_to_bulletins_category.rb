7e7eec34ce77d808324051c1698c70b78d7227b7
Arina Zaitseva <avanero@bk.ru>
2024-09-19 00:45:09 -0400

    fix github actions

--- db/migrate/20240917193652_add_not_null_constratint_to_bulletins_category.rb
+++ db/migrate/20240917193652_add_not_null_constratint_to_bulletins_category.rb
@@ -1,6 +1,9 @@
 class AddNotNullConstratintToBulletinsCategory < ActiveRecord::Migration[7.2]
   def change
-    Bulletin.where(category_id: nil).update_all(category_id: Category.first.id)
+    if Bulletin.any? && Category.any?
+      Bulletin.where(category_id: nil).update_all(category_id: Category.first.id)
+    end
+
     change_column_null(:bulletins, :category_id, false)
   end
 end
